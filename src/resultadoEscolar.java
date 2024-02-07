2d734ebd6bd8aec6651730620fe75078f1aa05cf
wellington <wellington.h.s.oliveira@hotmail.com>
2024-02-06 22:11:35 -0300

    condicional encadeada

--- src/resultadoEscolar.java
+++ src/resultadoEscolar.java
@@ -1,12 +1,17 @@
 public class resultadoEscolar {
     public static void main(String[] args) {
-        int nota = 6;
+        int nota = 4;
 
         if (nota >= 7)
-            System.out.println("o aluno esta aprovado");
-        
-        else
+           
+        System.out.println("o aluno esta aprovado");
 
+        else if (nota >= 5 && nota <7)
+            
+        System.out.println("Prova de Recuperação");
+
+        else
+            
         System.out.println("O aluno esta reprovado");
 
             
