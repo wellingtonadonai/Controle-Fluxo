25c0f1f4ab7ebe4b1ab5a1fca24af9f099d32a17
wellington <wellington.h.s.oliveira@hotmail.com>
2024-02-06 23:00:12 -0300

    Switch case

---
+++ src/planoOperadora.java
@@ -0,0 +1,24 @@
+public class planoOperadora {
+    public static void main(String[] args) {
+        
+         String sigla = "M";
+
+        switch (sigla) {
+
+            case "T" :{
+            System.out.println("5GB YOUTUBE");
+            
+            
+        }
+            case "M":{
+            System.out.println("Whatsapp e Instagram Gratis");
+            
+            
+        }
+            case "B":{
+            System.out.println("100 minutos de ligação");
+            
+        }
+    }
+}
+}
---
+++ src/sistemaDeMedida.java
@@ -0,0 +1,40 @@
+public class sistemaDeMedida {
+    public static void main(String[] args) {
+        String sigla = "A";
+
+        switch (sigla) {
+
+            case "P" :{
+            System.out.println("PEQUENO");
+            break;
+            
+        }
+            case "M":{
+            System.out.println("MEDIO");
+            break;
+            
+        }
+            case "G":{
+            System.out.println("GRANDE");
+            break;
+        }
+
+                        default: 
+            System.out.println("INDEFINIDO");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+            
+        
+    }
+}
