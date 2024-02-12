6dfa356b13a43986fb289b5f710bf993fa0c8d39
wellington <wellington.h.s.oliveira@hotmail.com>
2024-02-12 00:39:36 -0300

    exemplos de for e arrays

---
+++ src/ExemploForarrays.java
@@ -0,0 +1,11 @@
+    public class ExemploForarrays {
+        public static void main(String[] args) {
+            String alunos [] = {"Felipe","wellington", "patricia", "joao miguel"};
+
+            for (int x =0; x < alunos.length; x++) {
+                
+                System.out.println ("o aluno no indice x = " + x + " é " + alunos [ x ]);
+
+            }
+        }
+    }
---
+++ src/exemploFor.java
@@ -0,0 +1,9 @@
+public class exemploFor {
+
+    public static void main(String[] args) {
+        for (int carneirinho = 1; carneirinho <= 20; carneirinho ++){
+            System.out.println("contando carneirinhos " + carneirinho);
+        }
+        System.out.println("joaozinho dormiu");
+    }
+}
